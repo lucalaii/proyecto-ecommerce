7a634fff85c9eae48edff9c57f8d667c9f86d216
lucalai <lucaslaiz1245@gmail.com>
2024-06-18 16:39:18 -0300

    config de typeORM

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,11 +4,11 @@ import { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST } from './envs';
 
 const config = {
   type: 'postgres',
-  database: DB_NAME,
-  host: DB_HOST,
-  port: DB_PORT as unknown as number,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
+  database: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'lucas1234',
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
