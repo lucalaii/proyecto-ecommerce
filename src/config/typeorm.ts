6a58037d063a0dc3985b55c64c845b51f70a9a1b
lucalai <lucaslaiz1245@gmail.com>
2024-06-20 10:32:13 -0300

    agrego ssl a la config de typeorm

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,16 +4,19 @@ import { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST } from './envs';
 
 const config = {
   type: 'postgres',
-  database: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'lucas1234',
+  database: DB_NAME,
+  host: DB_HOST,
+  port: DB_PORT as unknown as number,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
   logging: false,
   synchronize: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   // dropSchema: true,
 };
 
